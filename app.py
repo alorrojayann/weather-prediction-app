29318f5b4762f077c99ed26adaac033454b8d5da
Jay-ann Alorro <157359579+alorrojayann@users.noreply.github.com>
2025-05-13 23:20:48 +0800

    update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ model = load_model("weather_model.h5")
 st.title("Weather-Based Ventilation Control")
 st.write("Enter the last 7 temperature max values to predict the next one.")
 
-# Input fields for the last 10 temp_max values
+# Input fields for the last 7 temp_max values
 temp_inputs = []
 for i in range(7):
     val = st.number_input(f"Temperature (°C) - Day {i+1}", min_value=-50.0, max_value=50.0, value=15.0)
@@ -26,6 +26,6 @@ if st.button("Predict"):
 
     # Ventilation logic
     if predicted_temp > 25:
-        st.success("Ventilation Status: OPEN - It's going to be warm.")
+        st.success("Ventilation Status: OPEN (It's going to be warm!)")
     else:
-        st.info("Ventilation Status: CLOSED - Cool or Moderate weather.")
+        st.info("Ventilation Status: CLOSED (Cool or moderate weather)")
